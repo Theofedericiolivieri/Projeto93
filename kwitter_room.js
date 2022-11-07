b83adc2b6e7ea83d8103f292cb84f9859795c578
Theofedericiolivieri <112435873+Theofedericiolivieri@users.noreply.github.com>
2022-11-07 18:04:51 -0300

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAcJoutwha0uSKPkovQcPxbuxGuchugG6I",
+    authDomain: "vamosconversardb.firebaseapp.com",
+    databaseURL: "https://vamosconversardb-default-rtdb.firebaseio.com",
+    projectId: "vamosconversardb",
+    storageBucket: "vamosconversardb.appspot.com",
+    messagingSenderId: "491859030837",
+    appId: "1:491859030837:web:385329d6d0e6a8b153527c"
+  };
