b23791cf75eca19e0dfbb6921c5eae4391e977af
Theofedericiolivieri <112435873+Theofedericiolivieri@users.noreply.github.com>
2022-11-09 18:50:13 -0300

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -7,3 +7,34 @@ const firebaseConfig = {
     messagingSenderId: "491859030837",
     appId: "1:491859030837:web:385329d6d0e6a8b153527c"
   };
+  firebase.initializeApp(firebaseConfig);
+  user_name= localStorage.getItem("user_name");
+  document.getElementById("user_name").innerHTML= "Bem-Vindo " + user_name + "!";
+function addRoom()
+{
+  roomName=document.getElementById("roomName").value;
+  firebase.database().ref("/").child(roomName).update({
+    purpose: "adding room name"
+  });
+  localStorage.setItem("roomName", roomName);
+  window.location= "kwitter_page.html";
+}
+
+function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+       roomNames = childKey;
+       console.log("Nome da Sala - " + roomNames);
+      row = "<div class='roomName' id="+roomNames+" onclick='redirectToRoomName(this.id)' >#"+ roomNames +"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+    });
+  });
+
+}
+
+getData();
+
+function redirectToRoomName(name)
+{
+ console.log(name);
+ localStorage.setItem("roomName", name);
+ window.location="kwitter_page.html";
+}
