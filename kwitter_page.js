92ce0b0af95743ce31c639ae041b67172af96d24
Theofedericiolivieri <112435873+Theofedericiolivieri@users.noreply.github.com>
2022-11-21 17:40:20 -0300

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -32,10 +32,13 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
      like = message_data['like'];
      name_with_tag = "<h4> "+ name +"<img class='user_tick' src='tick.png'></h4>";
      message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
+     likeButton="<button class='btn btn-warning' id="+ firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
+span_with_tag="<span class='glyphicon glyphicon-thumbs-up'>like: "+ like +"</span></button><hr>";
 // Fazer o botão de like
 // Fazer o ícone de like
 
-    row = //Juntar tudo na variável   
+    row =name_with_tag+message_with_tag+likeButton+span_with_tag;
+    document.getElementById("output").innerHTML+=row //Juntar tudo na variável   
     // Colocar a variável row no html
 //End code
   } });  }); }
@@ -45,14 +48,21 @@ function updateLike(message_id)
 {
 console.log("clicked on like button - " + message_id);
 button_id = message_id;
+likes=document.getElementById(button_id).value;
+updated_likes=Number(likes)+ 1;
 //Variável para o número atual de likes
 // Variável que vai armazenar o número de likes incrementado
 console.log(updated_likes);
 
 // Firebase
+firebase.database().ref(room_name).child(message_id).update({
+  like: updated_likes
+});
 
 }
 
 function logout() {
-
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("roomName");
+  window.location.replace("index.html");
 }
